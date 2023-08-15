d3ad6d88946c4c1315bfb15173f0be7a496ce9f3
Michael Graczyk <michael@mgraczyk.com>
2023-08-15 12:50:27 -0700

    Update git user

--- health-check.sh
+++ health-check.sh
@@ -60,9 +60,8 @@ done
 
 if [[ $commit == true ]]
 then
-  # Let's make Vijaye the most productive person on GitHub.
-  git config --global user.name 'Vijaye Raji'
-  git config --global user.email 'vijaye@statsig.com'
+  git config --global user.name 'Quilt Founders'
+  git config --global user.email 'founders@quilt.app'
   git add -A --force logs/
   git commit -am '[Automated] Update Health Check Logs'
   git push
